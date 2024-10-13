316e151a3e6f5a013588a20ad6aa9c7499693c88
wetgus <wetgusbetting@gmail.com>
2024-10-14 00:03:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 
-TELEGRAM_BOT_TOKEN = '7890352951:AAGL59jcPMgHDrCLL3HDPVDRSWPQrDuYYR0'  # Replace with your actual API token
+YOUR_BOT_TOKEN = '7890352951:AAGL59jcPMgHDrCLL3HDPVDRSWPQrDuYYR0'  # Replace with your actual API token
 ADMIN_USER_ID = 669208240  # Replace with your actual user ID
 
