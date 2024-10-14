c85e37f0b4dca78bef46b62c616c4512b477c38a
wetgus <wetgusbetting@gmail.com>
2024-10-14 18:25:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,7 @@
 
 API_TOKEN = '7890352951:AAGL59jcPMgHDrCLL3HDPVDRSWPQrDuYYR0'  # Replace with your actual API token
 ADMIN_USER_ID = 669208240  # Replace with your actual user ID
+
+# Firebase Configuration
+FIREBASE_KEY_PATH = 'https://github.com/wetgus/telegram-betting-bot/blob/main/firebase_key.json'  # Path to your Firebase key file
+FIREBASE_DATABASE_URL = 'https://bettingbot-6dbb1.firebaseio.com/'  # Your Firebase database URL
