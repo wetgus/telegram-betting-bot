0896071af2a4af2e4431bad1b9c94f3f9961e1cb
wetgus <wetgusbetting@gmail.com>
2024-10-14 21:27:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 API_TOKEN = '7890352951:AAGL59jcPMgHDrCLL3HDPVDRSWPQrDuYYR0'  # Replace with your actual API token
 ADMIN_USER_ID = 669208240  # Replace with your actual user ID
 MONGODB_URI = "mongodb+srv://wetgusbetting:TkfsEmL75Ue9aa8V@cluster0.3gl5y.mongodb.net/?retryWrites=true&w=majority&ssl=true"
-MONGODB_DATABASE = "BotBets"  # Change to your database name
+MONGODB_DATABASE = "BetsBot"  # Change to your database name
 MONGODB_COLLECTION = "Bets"  # Change to your collection name
