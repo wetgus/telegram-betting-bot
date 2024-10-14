71f3a4f4719c0c51ead2512d707371fd23a135b3
wetgus <wetgusbetting@gmail.com>
2024-10-14 19:49:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 
 API_TOKEN = '7890352951:AAGL59jcPMgHDrCLL3HDPVDRSWPQrDuYYR0'  # Replace with your actual API token
 ADMIN_USER_ID = 669208240  # Replace with your actual user ID
-MONGODB_URI = "mongodb+srv://wetgusbetting:TkfsEmL75Ue9aa8V@cluster0.3gl5y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGODB_URI = "mongodb+srv://wetgusbetting:<db_password>@cluster0.3gl5y.mongodb.net/?retryWrites=true&w=majority&ssl=true"
