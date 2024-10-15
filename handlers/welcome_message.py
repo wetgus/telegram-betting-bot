a29e634a97b495b74fb7ba386017813ce2b67184
wetgus <wetgusbetting@gmail.com>
2024-10-15 21:57:11 +0300

    Create welcome_message.py

---
+++ handlers/welcome_message.py
@@ -0,0 +1,8 @@
+# handlers/welcome_message.py
+
+WELCOME_MESSAGE = (
+    "I am Atlaslive Sportsbook bot. Let's explore my functions:\n"
+    "/create_bet - initiates bet creation\n"
+    "/balance - displays your current balance\n"
+    "/help - shows available commands\n"
+)
