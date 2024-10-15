cc39434e45f0ce0d6e4e24b3b81f7fa79fe7fa93
wetgus <wetgusbetting@gmail.com>
2024-10-15 20:22:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ ADMIN_USER_ID = 669208240  # Replace with your actual user ID
 MONGODB_URI = "mongodb+srv://wetgusbetting:TkfsEmL75Ue9aa8V@cluster0.3gl5y.mongodb.net/?retryWrites=true&w=majority&ssl=true"
 MONGODB_DATABASE = "BetsBot"  # Change to your database name
 MONGODB_COLLECTION = "Bets"  # Change to your collection name
-PREDEFINED_BETS_COLLECTION = "Markets"
+PBETS_COLLECTION = "Markets"
